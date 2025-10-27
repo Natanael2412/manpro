5ec7fa7c8ae2f1cdc9d2ee6ee519cb8fa284b70b
Natanael2412 <149650461+Natanael2412@users.noreply.github.com>
2025-10-27 09:26:34 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+// Dapatkan elemen tombol dan menu
+const menuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
 
+// Tambahkan event listener 'click' pada tombol
+menuButton.addEventListener('click', () => {
+    // Toggle class 'hidden' pada menu mobile
+    mobileMenu.classList.toggle('hidden');
+});
