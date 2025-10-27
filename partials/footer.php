5699fd9492c318f6824c0d178e93235f9faa97e7
Natanael2412 <149650461+Natanael2412@users.noreply.github.com>
2025-10-27 09:26:10 +0700

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -1,1 +1,10 @@
+</main>
+    <footer class="bg-gray-800 text-white mt-12">
+        <div class="container mx-auto px-6 py-6 text-center">
+            <p>&copy; <?php echo date("Y"); ?> [Nama Prodi Anda], [Nama Universitas Anda].</p>
+        </div>
+    </footer>
 
+    <script src="js/main.js"></script>
+</body>
+</html>
